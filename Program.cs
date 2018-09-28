3378fc37ca3bb3086348f6dddbcce7a259c07f70
OscarAbrahamH <43403654+OscarAbrahamH@users.noreply.github.com>
2018-09-28 09:20:49 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_1_HUERTA_MADUEÑO_OSCAR_ABRAHAM
+{
+    class FACTORIAL
+    {
+        public int Numero;
+        public int Resultado = 1;
+
+        public void FactorValor()
+        {
+            Numero = 6;
+            Console.WriteLine(" ");
+        }
+
+       public void ProcesoFOR()
+        {
+            
+            for (int X = 1; X <= Numero; X++)
+            {
+                Resultado = Resultado * X;
+                Console.Write(X + " * ");
+            }   
+        }
+
+        public void Imprimir()
+       {
+           Console.WriteLine(" ");
+           Console.WriteLine("------------------------------------------");
+           Console.WriteLine(" ");
+           Console.WriteLine("Factorial de " + Numero + " = " + Resultado);
+           Console.WriteLine(" ");
+           Console.WriteLine("------------------------------------------");
+
+       }
+   
+        static void Main(string[] args)
+        {
+            FACTORIAL figura1 = new FACTORIAL();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            
+            figura1.FactorValor();
+            figura1.ProcesoFOR();
+            figura1.Imprimir();
+
+            stopWatch.Stop();
+            Console.WriteLine("Proceso Finalizado en " +
+            stopWatch.Elapsed.ToString(), "Mensaje Sistema");
+            Console.ReadKey();
+        }
+    }
+}
+
+
